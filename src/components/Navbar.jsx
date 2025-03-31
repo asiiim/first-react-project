3198254ce2529f8006406a8d43adb73684c684f9
Aashim Bajracharya <ashimbazracharya@gmail.com>
2025-03-31 18:45:03 +0545

    modify nav bar
    - For semantic HTML purposes, the Navbar should render
        a <header> with a <nav> nested inside.
    - The image and "ReactFacts" text elements can
        both be rendered as children inside the <nav>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,10 @@
 export default function Navbar() {
     return (
-        <h1>Navbar component here</h1>
+        <header>
+            <nav>
+                <img src="/react-logo.png" alt="React Logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
     )
 }
